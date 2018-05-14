469e1ec44f425394c0b080da2c6de5cbd634b10d
Luciana <lprimante@gmail.com>
2018-05-14 16:25:20 -0300

    Adicionado comentários sobre o código

--- main.js
+++ main.js
@@ -7,13 +7,16 @@ form.addEventListener('submit', function(event) {
   var tarefa = campo.value;
 
   if(tarefa.length < 1) {
+    // if(tarefa !== "") {  ------------>>>>> Outra forma de fazer a comparação
     alert("Por favor, preencha um valor válido");
   } else { 
     var novaLinha = document.createElement("li");
     novaLinha.textContent = tarefa;
+    //var novaTarefa = document.createTextNode(tarefa)  ------------->>>>> Outra forma de incluir texto no DOM 
     lista.appendChild(novaLinha);
     form.reset();
-    
+    // campo.value = ""  ------------------------>>> Outra forma de limpar o form
+
     novaLinha.addEventListener('click', function(event) {
       if(event.target.classList.toggle('done')){
         event.target.classList.add('done');
