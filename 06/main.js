8a79a2627ff715ebcbf6a8dfae3c6715cf9da929
Luciana <lprimante@gmail.com>
2018-05-23 13:32:50 -0300

    Exercicio 06

--- 06/main.js
+++ 06/main.js
@@ -2,39 +2,48 @@ const form = document.querySelector('form')
 const inputResultado = form.querySelector('input')
 
 form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const idResultado = document.querySelector('#results');
-    if (idResultado.childNodes) {
-        console.log(idResultado)
-        // idResultado.childNodes.removeChild(NodeList);
-      }
-
-    const inputValue = inputResultado.value;
-   
- 
-    getData("https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=" + inputValue + "&key=################################").then(function(response) {
+// inputResultado.addEventListener('keyup', function(event) { 
+    event.preventDefault()
+    const idResultado = document.querySelector('#results')
+    
+    while (idResultado.firstChild) {
+        idResultado.removeChild(idResultado.firstChild)
+      } 
+     
+    const inputValue = inputResultado.value
+    if (inputResultado.value === "") {
+        idResultado.innerHTML = "<p>Digite uma pesquisa válida</p>"
+    } else {   getData(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${inputValue}&key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`).then(function(response) {
     const jsonResponse = JSON.parse(response)
     // console.log(response)
     const arrayResponse = jsonResponse.items
+        if (jsonResponse.items.length === 0) {
+            idResultado.innerHTML = "<p>Nenhum resultado encontrado</p>"
+        } else {
+            for (const video of arrayResponse) {
+            console.log(video)            
+                const link = document.createElement("a")
+                const h1 = document.createElement("h1")
+                const p = document.createElement("p")
+                const foto = document.createElement("img")
+                
 
-        for (const video of arrayResponse) {
-        console.log(video)            
-            const link = document.createElement("a")
-            const h1 = document.createElement("h1")
-            const p = document.createElement("p")
+                link.setAttribute("href", `https://youtube.com/watch?v=${video.id.videoId}`)
+                h1.textContent = video.snippet.title
+                p.textContent = video.snippet.description
+                foto.src = video.snippet.thumbnails.high.url
 
-            link.setAttribute("href", ("https://youtube.com/watch?v=" + video.id.videoId))
-            h1.textContent = video.snippet.title
-            p.textContent = video.snippet.description
-
-            idResultado.appendChild(link)
-            link.appendChild(h1)
-            link.appendChild(p)
-        
-        }  
+                idResultado.appendChild(link)
+                link.appendChild(foto)
+                link.appendChild(h1)
+                link.appendChild(p)
+            }  
+        }
         }, function(error) { 
-            container.innerHTML = error
+            idResultado.innerHTML = `<p>${error}</p>`
         })
+    }
 
     form.reset();
+
 })
